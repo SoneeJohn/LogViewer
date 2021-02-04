45d4ca85a9e1011bd1ba58dcd3f77782c5f37a0e
Soneé John <sonee@alphasoftware.co>
2021-02-03 23:56:59 -0400

    Initial Commit

---
+++ LogViewer/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  LogViewer
+//
+//  Created by Soneé John on 03/02/2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
