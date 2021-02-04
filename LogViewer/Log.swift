952957c61078024b0012189dd2361273f2205fc1
Soneé John <sonee@alphasoftware.co>
2021-02-04 03:59:24 -0400

    Add `LogReaderOperation` and other models

---
+++ LogViewer/Log.swift
@@ -0,0 +1,136 @@
+//
+//  Log.swift
+//  LogViewer
+//
+//  Created by Soneé John on 04/02/2021.
+//
+
+import Foundation
+
+struct Log {
+    let accounts: [Account]
+    let calendars: [Calendar]
+    let syncQueue: [SyncQueue]
+    let logFileURL: URL
+    
+    init?(logFileURL: URL, accountStrings: [String], calendarStrings: [String], syncQueueStrings: [String]) {
+        guard accountStrings.isEmpty == false else { return nil }
+        guard calendarStrings.isEmpty == false else { return nil }
+        guard syncQueueStrings.isEmpty == false else { return nil }
+        
+        accounts = accountStrings.compactMap{ Account($0) }
+        calendars = calendarStrings.compactMap{ Calendar($0) }
+        syncQueue = syncQueueStrings.compactMap { SyncQueue($0) }
+        self.logFileURL = logFileURL
+    }
+}
+
+struct Calendar {
+    let name: String
+    let identifier: String
+    private let string: String
+    init?(_ string: String) {
+        guard let stringWithoutDate = string.split(separator: "\t").last else { return nil }
+        self.string = string
+        
+        let properties = stringWithoutDate.split(separator: ",").compactMap { (sub) -> String in
+            return String(sub).trimmingCharacters(in: .whitespacesAndNewlines)
+        }
+        guard properties.count >= 2 else { return nil }
+        
+        self.name = properties[0]
+        self.identifier = properties[1]
+    }
+}
+
+struct Account {
+    private let string: String
+    let name: String
+    let identifier: String
+    let state: Bool
+    let framework: String
+    init?(_ string: String) {
+        guard let stringWithoutDate = string.split(separator: "\t").last else { return nil }
+        self.string = string
+        
+        let properties = stringWithoutDate.split(separator: ",").compactMap { (sub) -> String in
+            return String(sub).trimmingCharacters(in: .whitespacesAndNewlines)
+        }
+        guard properties.count >= 4 else { return nil }
+        
+        self.name = properties[0]
+        self.identifier = properties[1]
+        self.state = properties[2].contains("enabled")
+        self.framework = properties[3]
+    }
+}
+
+struct SyncQueue {
+    let name: String
+    let identifier: String
+    private let string: String
+    init?(_ string: String) {
+        guard let properties = SyncQueue.properties(string) else { return nil }
+        
+        self.name = properties[0]
+        self.identifier = properties[1]
+        self.string = string
+    }
+    
+    static func properties(_ string: String) -> [String]? {
+        var properties: [String] = []
+        
+        if string.contains("Sync queues:") {
+            guard let firstGroup = string.groups(for: #"(?:[^\\\\s]+)s: ([\S]+) \/ ([^(\s]+)"#).first else { return nil }
+            guard firstGroup.count >= 3 else { return nil }
+            properties.append(firstGroup[1])
+            properties.append(firstGroup[2])
+            return properties
+        }
+        
+        guard let firstGroup = string.groups(for: #"([\S]+) \/ (\S*)"#).first else { return nil }
+        guard firstGroup.count >= 3 else { return nil }
+        properties.append(firstGroup[1])
+        properties.append(firstGroup[2])
+        
+        guard properties.isEmpty == false else { return nil }
+        return properties
+    }
+}
+
+extension String {
+    func matches(for regex: String, options: NSRegularExpression.Options = NSRegularExpression.Options()) -> [String] {
+        do {
+            let regex = try NSRegularExpression(pattern: regex, options: options)
+            let results = regex.matches(in: self,
+                                        range: NSRange(self.startIndex..., in: self))
+            return results.map {
+                String(self[Range($0.range, in: self)!])
+            }
+        } catch let error {
+            print("Invalid regex: \(error.localizedDescription)")
+            return []
+        }
+    }
+    
+    func groups(for regexPattern: String, options: NSRegularExpression.Options = NSRegularExpression.Options()) -> [[String]] {
+        do {
+            let text = self
+            let regex = try NSRegularExpression(pattern: regexPattern, options: options)
+            let matches = regex.matches(in: text,
+                                        range: NSRange(text.startIndex..., in: text))
+            return matches.map { match in
+                return (0..<match.numberOfRanges).map {
+                    let rangeBounds = match.range(at: $0)
+                    guard let range = Range(rangeBounds, in: text) else {
+                        return ""
+                    }
+                    return String(text[range])
+                }
+            }
+        } catch let error {
+            print("Invalid regex: \(error.localizedDescription)")
+            return []
+        }
+    }
+}
---
+++ LogViewer/LogReaderOperation.swift
@@ -0,0 +1,127 @@
+//
+//  LogReaderOperation.swift
+//  LogViewer
+//
+//  Created by Soneé John on 03/02/2021.
+//
+
+import Foundation
+import Zip
+
+class LogReaderOperation: Operation {
+    
+    override var isAsynchronous: Bool { return true }
+    override var isFinished: Bool { return state == .finished }
+    override var isExecuting: Bool { return state == .executing }
+    
+    private var _logs: [Log]?
+    public var logs: [Log]? {
+        return _logs
+    }
+    private enum State: String {
+        case ready = "Ready"
+        case executing = "Executing"
+        case finished = "Finished"
+        fileprivate var keyPath: String { return "is" + self.rawValue }
+    }
+    
+    private var state: State = .ready {
+        willSet {
+            willChangeValue(forKey: state.keyPath)
+            willChangeValue(forKey: newValue.keyPath)
+        }
+        didSet {
+            didChangeValue(forKey: state.keyPath)
+            didChangeValue(forKey: oldValue.keyPath)
+        }
+    }
+    
+    public let zipFileURLPath: URL
+    
+    init(zipFileURLPath: URL) {
+        self.zipFileURLPath = zipFileURLPath
+        super.init()
+    }
+    
+    override func start() {
+        guard isFinished == false else { return }
+        
+        let directory = URL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(ProcessInfo.processInfo.globallyUniqueString)
+        guard ((try? Zip.unzipFile(zipFileURLPath, destination: directory, overwrite: false, password: nil)) != nil) else { return }
+        let logsDirectory = directory.appendingPathComponent("Logs")
+        
+        guard let contents = try? FileManager.default.contentsOfDirectory(atPath: logsDirectory.path) else { return }
+        guard contents.contains("com.flexibits.fantastical2.mac") else { return }
+        let mainAppLogDirectory = logsDirectory.appendingPathComponent("com.flexibits.fantastical2.mac")
+        
+        finish(logs: LogReaderOperation.processLogs(directoryURL: mainAppLogDirectory))
+    }
+    
+    func finish(logs: [Log]?) {
+        _logs = logs
+        state = .finished
+    }
+}
+
+extension LogReaderOperation {
+    static func processLogs(directoryURL: URL) -> [Log]? {
+        guard let contents = try? FileManager.default.contentsOfDirectory(atPath: directoryURL.path) else { return nil }
+       
+        var logURLs: [URL] = []
+        var logs: [Log] = []
+        contents.forEach { (logFileName) in
+            let url = directoryURL.appendingPathComponent(logFileName)
+            guard url.pathExtension == "log" else { return }
+            logURLs.append(url)
+        }
+        
+        logURLs.forEach { (logURL) in
+            guard let contents = try? String(contentsOf: logURL) else { return }
+           
+            let lines = contents.split(separator: "\n")
+            enum ProcessingType {
+                case accounts
+                case calendars
+                case syncQueues
+                case none
+            }
+            var type: ProcessingType = .none
+            
+            var accountLines: [String] = []
+            var calendarLines: [String] = []
+            var syncQueueLines: [String] = []
+            
+            lines.forEach { (line) in
+                if line.contains("Accounts:") {
+                    type = .accounts
+                } else if line.contains("Calendars:") {
+                    type = .calendars
+                } else if line.contains("Sync queues:") {
+                    type = .syncQueues
+                }
+                
+                switch type {
+                case .accounts:
+                    if line.contains("Accounts:") == false {
+                        accountLines.append(String(line))
+                    }
+                case .calendars:
+                    if line.contains("Calendars:") == false {
+                        calendarLines.append(String(line))
+                    }
+                case .syncQueues:
+                    if line.contains("FBSyncQueue") || line.contains(")>") {
+                        syncQueueLines.append(String(line))
+                    }
+                default: break
+                }
+            }
+            
+            guard let log = Log(logFileURL: logURL, accountStrings: accountLines, calendarStrings: calendarLines, syncQueueStrings: syncQueueLines) else { return }
+            logs.append(log)
+        }
+        
+        guard logs.isEmpty == false else { return nil }
+        return logs
+    }
+}
